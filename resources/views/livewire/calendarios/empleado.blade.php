cfba0ea614c0eee7bd3c7283c350c78cb474aa2c
Matías <matiasagbenitez@gmail.com>
2022-11-28 12:01:40 -0300

    Frontend en el marcado de asistencia

--- resources/views/livewire/calendarios/empleado.blade.php
+++ resources/views/livewire/calendarios/empleado.blade.php
@@ -1,8 +1,11 @@
-<div>
+<div class="max-w-3xl mx-auto p-8 mt-12 bg-white rounded-lg shadow-lg">
     @if ($empleado)
-        Empleado: {{$empleado->nombre}} {{$empleado->apellido}}
-        <x-jet-danger-button wire:click="marcarAsistencia">
-            {{ $asistencia->fecha_hora_salida ? "Marcar entrada" : "Marcar salida" }}
-        </x-jet-danger-button>
+        <div class="space-y-3 text-center">
+            <h1 class="font-bold text-3xl">¡Hola, {{ $empleado->nombre }} {{ $empleado->apellido }}!</h1>
+            <h2 class="font-bold text-lg">Marcar entrada/salida</h2>
+            <x-jet-danger-button wire:click="marcarAsistencia">
+                {{ $asistencia->fecha_hora_salida ? 'Marcar entrada' : 'Marcar salida' }}
+            </x-jet-danger-button>
+        </div>
     @endif
 </div>
