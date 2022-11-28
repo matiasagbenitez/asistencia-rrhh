e40ef928d49b38b07e678b9c051f47aed9a3bfe3
Matías <matiasagbenitez@gmail.com>
2022-11-28 02:06:07 -0300

    Calendario inicial

---
+++ app/Http/Livewire/Calendar/Calendar.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Livewire\Calendar;
+
+use App\Models\Asistencia;
+use Livewire\Component;
+
+class Calendar extends Component
+{
+    public $events = [];
+    public $year, $month;
+    public $daysNames = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+    public $selectedYear, $selectedMonth;
+
+    public function mount()
+    {
+        $this->years = range(2020, 2022);
+        $this->months = [
+            1 => 'Enero',
+            2 => 'Febrero',
+            3 => 'Marzo',
+            4 => 'Abril',
+            5 => 'Mayo',
+            6 => 'Junio',
+            7 => 'Julio',
+            8 => 'Agosto',
+            9 => 'Septiembre',
+            10 => 'Octubre',
+            11 => 'Noviembre',
+            12 => 'Diciembre',
+        ];
+
+        $this->selectedYear = date('Y');
+        $this->selectedMonth = date('m');
+    }
+
+    public function render()
+    {
+        return view('livewire.calendar.calendar', [
+            'days' => $this->getDaysInMonth(),
+        ]);
+    }
+
+    public function getDaysInMonth()
+    {
+        $events = Asistencia::where('empleado_id', 5)->whereYear('fecha_hora_entrada', $this->selectedYear)
+            ->whereMonth('fecha_hora_entrada', $this->selectedMonth)
+            ->get();
+
+        $daysOfMonth = cal_days_in_month(CAL_GREGORIAN, $this->selectedMonth, $this->selectedYear);
+        $firstDay = $this->selectedYear . '-' . $this->selectedMonth . '-01';
+        $dayOfWeek = date('w', strtotime($firstDay));
+
+        $days[] = [
+            'day' => '',
+            'events' => [],
+        ];
+
+        for ($i = 0; $i < $dayOfWeek; $i++) {
+            $days[] = [
+                'day' => '',
+                'events' => [],
+            ];
+        }
+
+        for ($i = 0; $i < $daysOfMonth; $i++) {
+            $days[] = [
+                'day' => $i + 1,
+                'events' => [],
+            ];
+        }
+
+        foreach ($events as $event) {
+            $diaEvento = date('j', strtotime($event->fecha_hora_entrada));
+            foreach ($days as $key => $day) {
+                if ($day['day'] == $diaEvento) {
+                    $days[$key]['events'][] = $event->cantidad_horas;
+                }
+            }
+        }
+
+        $lastDay = $this->selectedYear . '-' . $this->selectedMonth . '-' . $daysOfMonth;
+        $lastDayOfWeek = date('w', strtotime($lastDay));
+
+        $colsToAdd = 6 - $lastDayOfWeek;
+        for ($i = 0; $i < $colsToAdd; $i++) {
+            $days[] = [
+                'day' => '',
+                'events' => [],
+            ];
+        }
+
+        return $days;
+    }
+}
---
+++ resources/views/livewire/calendar/calendar.blade.php
@@ -0,0 +1,59 @@
+<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+
+    {{-- TÍTULO --}}
+    <div class="flex items-center justify-center gap-3">
+        <i class="fas fa-calendar text-2xl"></i>
+        <h1 class="text-2xl uppercase font-bold text-center">Calendario maestro</h1>
+    </div>
+
+    {{-- FILTROS --}}
+    <div class="flex gap-3 my-3">
+        <div class="w-1/2">
+            <select wire:model="selectedMonth" class="input-control w-full">
+                @foreach ($months as $month => $name)
+                    <option value="{{ $month }}">{{ $month }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="w-1/2">
+            <select wire:model="selectedYear" class="input-control w-full">
+                @foreach ($years as $year)
+                    <option value="{{ $year }}">{{ $year }}</option>
+                @endforeach
+            </select>
+        </div>
+    </div>
+
+    {{-- NOMBRES DE LOS DÍAS --}}
+    <div class="grid grid-cols-7 gap-1">
+        @foreach ($daysNames as $name)
+            <div class="text-center font-bold p-4 uppercase">
+                {{ $name }}
+            </div>
+        @endforeach
+    </div>
+
+    {{-- DÍAS --}}
+    <div class="grid grid-cols-7 gap-3">
+        @foreach ($days as $day)
+            @if (empty($day['day']))
+                <div class="aspect-square flex justify-center items-center">
+                    {{ $day['day'] }}
+                </div>
+            @else
+                <div class="aspect-square justify-center items-center bg-slate-100 hover:bg-slate-200 hover:scale-150 hover:text-xs p-3 rounded-lg">
+                    <p class="text-center font-bold mb-2">{{ $day['day'] }}</p>
+
+                    @foreach ($day['events'] as $event)
+                        <div class="bg-slate-300 p-1 rounded-md">
+                            <span>
+                                {{ $event }}
+                            </span>
+                        </div>
+                    @endforeach
+                </div>
+            @endif
+        @endforeach
+    </div>
+
+</div>
